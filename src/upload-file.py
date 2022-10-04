3e99fb3014bd0e786bafcb24e091f32c67409a18
Tomas Szwarcberg <tomas.szwarcberg@innovid.com>
2022-10-04 17:50:04 -0300

    Upload client now uses ClientFactory instead of manually instanciating each client

--- src/upload-file.py
+++ src/upload-file.py
@@ -3,32 +3,29 @@ from lib.client.parsing import upload_args
 from lib.client.logger import *
 from lib.client.GBNClient import GBNClient
 from lib.client.SaWClient import SaWClient
+from lib.client.ClientFactory import ClientFactory
+from lib.server.exceptions.ProtocolNotSupportedException import ProtocolNotSupportedException
+
 
 LOG_FILE = "upload_log.txt"
 
 
-def main(args):
+def main(args, client_factory):
     init_logger(LOG_FILE)
-
     path = args.src + args.name
 
-    if args.protocol.lower() == "tcp":
-        tcp_client = TCPClient()
+    try:
+        tcp_client = client_factory.get_client_by_protocol(args.protocol)
         tcp_client.start_upload(args.host, path, args.port, args)
-
-    elif args.protocol.lower() == "saw":
-        saw_client = SaWClient()
-        saw_client.start_upload(args.host, path, args.port, args)
-
-    elif args.protocol.lower() == "gbn":
-        gbn_client = GBNClient()
-        gbn_client.start_upload(args.host, path, args.port, args)
-
-    else:
+    except ProtocolNotSupportedException as e:
+        print(f"Protocol not supported exception: {e}")
         log_protocol_error(args.protocol)
+    except Exception as e:
+        print(f"Exception: {e}")
 
 
 if __name__ == "__main__":
     args = upload_args()
-    main(args)
+    client_factory = ClientFactory()
+    main(args, client_factory)
 
