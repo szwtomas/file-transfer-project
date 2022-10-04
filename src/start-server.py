1665ecba2a42aea4b67a8a0a083f01d1399b4c99
Tomas Szwarcberg <tomas.szwarcberg@innovid.com>
2022-10-04 14:21:01 -0300

    Creates and uses ClientFactory for both upload and download clients

--- src/start-server.py
+++ src/start-server.py
@@ -2,6 +2,8 @@ from lib.server.parsing import server_args
 from lib.server.constants import LOG_FILE_PATH
 from lib.server.logger import init_logger
 from lib.server.ProtocolServerFactory import ProtocolServerFactory
+from lib.server.exceptions.ProtocolNotSupportedException import ProtocolNotSupportedException
+
 
 def start_logging(protocol_logger):
     init_logger(LOG_FILE_PATH)
@@ -11,9 +13,11 @@ def start_logging(protocol_logger):
 def main(host, port, storage, protocol, args, protocol_server_factory):
     init_logger(LOG_FILE_PATH)
     start_logging(protocol_server_factory.get_protocol_logger(protocol))
-    server = protocol_server_factory.get_server_from_selected_protocol(protocol)
-    server.run()
-
+    try:
+        server = protocol_server_factory.get_server_from_selected_protocol(protocol)
+        server.run()
+    except ProtocolNotSupportedException as e:
+        print(f"Protocol not supported exception: {e}")
 
 
 if __name__ == "__main__":
--- src/upload-file.py
+++ src/upload-file.py
@@ -3,32 +3,27 @@ from lib.client.parsing import upload_args
 from lib.client.logger import *
 from lib.client.GBNClient import GBNClient
 from lib.client.SaWClient import SaWClient
+from lib.server.exceptions.ProtocolNotSupportedException import ProtocolNotSupportedException
+from lib.client.ClientFactory import ClientFactory
+
 
 LOG_FILE = "upload_log.txt"
 
 
-def main(args):
+def main(args, client_factory):
     init_logger(LOG_FILE)
-
     path = args.src + args.name
 
-    if args.protocol.lower() == "tcp":
-        tcp_client = TCPClient()
-        tcp_client.start_upload(args.host, path, args.port, args)
-
-    elif args.protocol.lower() == "saw":
-        saw_client = SaWClient()
-        saw_client.start_upload(args.host, path, args.port, args)
-
-    elif args.protocol.lower() == "gbn":
-        gbn_client = GBNClient()
-        gbn_client.start_upload(args.host, path, args.port, args)
-
-    else:
+    try:
+        client = client_factory.get_client_by_protocol(args.protocol.lower())
+        client.start_upload(args.host, args.port, path, args.name)
+    except ProtocolNotSupportedException as e:
         log_protocol_error(args.protocol)
+        print(f"Protocol not supported exception: {e}")
 
 
 if __name__ == "__main__":
     args = upload_args()
-    main(args)
+    client_factory = ClientFactory()
+    main(args, client_factory)
 
