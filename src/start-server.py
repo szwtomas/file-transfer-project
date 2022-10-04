5991b2e1c1055d3a562732e112925cd6c3d5935d
agus815 <asu@fi.uba.ar>
2022-10-04 07:17:29 -0300

    feat: server start logging

--- src/start-server.py
+++ src/start-server.py
@@ -2,19 +2,31 @@ from lib.server.GBNServer import GBNServer
 from lib.server.Server import Server
 from lib.server.SAWServer import SAWServer
 from lib.server.parsing import server_args
+from lib.server.logger import *
+
+LOG_FILE = "server_log.txt"
 
 
-def main(host, port, storage, protocol):
+def main(host, port, storage, protocol, args):
+    init_logger(LOG_FILE)
+
     # We can optionally accept host and port as command line parameters in the future
     if protocol.lower() == "tcp":
-        server = Server(host, port, storage)
-    if protocol.lower() == "saw":
-        server = SAWServer(host, port, storage)
-    if protocol.lower() == "gbn":
-        server = GBNServer(host, port, storage)
+        log_tcp()
+        server = Server(host, port, storage, args)
+    elif protocol.lower() == "saw":
+        log_saw()
+        server = SAWServer(host, port, storage, args)
+    elif protocol.lower() == "gbn":
+        log_gbn()
+        server = GBNServer(host, port, storage, args)
+    else:
+        log_protocol_error(protocol)
+        return
+
     server.run()
 
 
 if __name__ == "__main__":
     args = server_args()
-    main(args.host, args.port, args.storage, args.protocol)
+    main(args.host, args.port, args.storage, args.protocol, args)
